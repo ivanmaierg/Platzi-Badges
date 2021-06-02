cd36f2d1a16d46fcb12b4d9514183db28ae8c53c
Getsuga743 <ivan_maier@hotmail.com>
2021-06-02 08:02:19 -0300

    2 files modified

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Badge from './components/Badge';
-import './global.css';
 import 'bootstrap/dist/css/bootstrap.css';
 ReactDOM.render(<Badge />, document.getElementById('root'));
