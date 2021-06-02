f250237a68ec8d6e00078e9b07a9bccd21a020bc
Getsuga743 <ivan_maier@hotmail.com>
2021-06-02 08:23:00 -0300

    3 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Import stylesheets
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import JS libs
+import $ from 'jquery';
+import 'bootstrap';
+
 import Badge from './components/Badge';
 
 ReactDOM.render(<Badge />, document.getElementById('root'));
