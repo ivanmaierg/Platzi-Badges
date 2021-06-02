148e123984549a2c073888e383690a3cd4b02c4f
Getsuga743 <ivan_maier@hotmail.com>
2021-06-02 08:34:38 -0300

    4 files modified

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,31 +1,30 @@
 import React from 'react';
 
-import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
-
+import './styles/Badge.css';
 class Badge extends React.Component {
   render() {
     return (
-      <div className="Badge">
-        <div className="Badge__header">
+      <div className="badge">
+        <div className="badge_header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div className="Badge__section-name">
+        <div className="badge_section-name">
           <img
-            className="Badge__avatar"
+            className="badge_avatar"
             src="https://www.gravatar.com/avatar/"
             alt="Avatar"
           />
-          <h1>
-            Richard <br /> Kaufman
+          <h1 className="text-dark">
+            Ivan <br /> Maier
           </h1>
         </div>
-        <div className="Badge__section-info">
-          <h3>Web dev</h3>
-          <div>@imaierg</div>
+        <div className="badge_section-info">
+          <h3 className="text-dark">Web dev</h3>
+          <div className="text-dark">@imaierg</div>
         </div>
-        <div className="Badge__footer">#platziconf</div>
+        <div className="badge_footer">#platziconf</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // Import stylesheets
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
