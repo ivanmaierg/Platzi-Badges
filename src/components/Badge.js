1488b973d56b4c5fc0e32f07b12de1b37f6226ff
Getsuga743 <ivan_maier@hotmail.com>
2021-06-02 07:58:15 -0300

    1 file modified, 4 added, 2 deleted

---
+++ src/components/Badge.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './styles/Badge.css';
+import confLogo from '../images/badge-header.svg';
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar/"
+            alt="Avatar"
+          />
+          <h1>
+            Richard <br /> Kaufman
+          </h1>
+        </div>
+        <div className="Badge__section-info">
+          <h3>Web dev</h3>
+          <div>@imaierg</div>
+        </div>
+        <div className="Badge__footer">#platziconf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import Badge from './components/Badge';
+import './global.css';
+ReactDOM.render(<Badge />, document.getElementById('root'));
