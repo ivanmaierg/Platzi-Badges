b235b9c2f8eb600083835cb809fbe6c8f13a2d75
ivanmaierg <ivan_maier@hotmail.com>
2021-06-02 13:23:31 -0300

    Bootstrap added.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Import stylesheets
-import 'bootstrap';
 import './index.css';
 
 // Import JS libs
 import 'bootstrap';
-
 import BadgeNew from './pages/BadgeNew';
 
 ReactDOM.render(<BadgeNew />, document.getElementById('root'));
